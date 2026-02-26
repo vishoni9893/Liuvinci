463b800eedb07a26fa58e37e235cc1111bcf2420
vishoni9893 <vishsoni4@gmail.com>
2026-02-26 16:34:37 +0530

    Sync latest Shopify theme

--- assets/product-form.js
+++ assets/product-form.js
@@ -103,7 +103,7 @@ if (!customElements.get('product-form')) {
             this.submitButton.classList.remove('loading');
             if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
             if (!this.error) this.submitButton.removeAttribute('aria-disabled');
-            this.querySelector('.loading__spinner').classList.add('hidden');
+            this.querySelector('.loading__spinner')?.classList.add('hidden');
 
             CartPerformance.measureFromEvent("add:user-action", evt);
           });
